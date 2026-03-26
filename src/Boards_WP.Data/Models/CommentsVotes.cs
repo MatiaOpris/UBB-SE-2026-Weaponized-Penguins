61583df97e1da7ceca7e08e3ccc5883f291689c3
razvanberbecar <razvan.berbecar@stud.ubbcluj.ro>
2026-03-26 10:24:58 +0200

    added Comment model, CommentsVotes model and VoteType enum  (#8)
    * added Comment model, CommentsVotes model and VoteType enum
    
    * added Comment model, CommentsVotes model and VoteType enum
    
    * set userCurrentVote to None in Comment class

---
+++ src/Boards_WP.Data/Models/CommentsVotes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Boards_WP.Data.Models;
+
+public class CommentsVotes
+{
+    private int userID { get; set; }
+    private int commentID { get; set; }
+    private VoteType vote { get; set; } = VoteType.None;
+
+}
